d3bf96ab63cf0b9008af0a026cdefe0e1b98e9eb
Bryan Weber <bryan.w.weber@gmail.com>
2022-05-19 13:58:54 -0400

    Return all the response keys to the model
    Pydantic will ignore unknown field names when creating the model. This
    lets us return all the keys from the response that we get, so that we
    can add new fields to the model in the future without modifying the
    request code. Unless, of course, the response from the external API
    needs some more munging. But nothing to be done about that.

--- app/external.py
+++ app/external.py
@@ -17,8 +17,8 @@ CROSSREF_HEADERS = {
 async def datacite_request(doi: str) -> dict[str, str]:
     def datacite_munger(
         *, doi: str, titles: list[dict[str, str]], **response: dict[str, Any]
-    ) -> dict[str, str]:
-        return {"doi": doi, "title": titles[0]["title"]}
+    ) -> dict[str, Any]:
+        return {"doi": doi, "title": titles[0]["title"], **response}
 
     async with httpx.AsyncClient(
         base_url=DATACITE_URL, headers=DATACITE_HEADERS
@@ -30,8 +30,8 @@ async def datacite_request(doi: str) -> dict[str, str]:
 async def crossref_request(doi: str) -> dict[str, str]:
     def crossref_munger(
         *, DOI: str, title: list[str], **response: dict[str, Any]
-    ) -> dict[str, str]:
-        return {"doi": DOI, "title": title[0]}
+    ) -> dict[str, Any]:
+        return {"doi": DOI, "title": title[0], **response}
 
     async with httpx.AsyncClient(
         base_url=CROSSREF_URL, headers=CROSSREF_HEADERS
